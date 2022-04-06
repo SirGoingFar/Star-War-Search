d7dad62b4dfab02de38ddaef3fdfdcf9dabc8518
Akintunde <akintunde_olanrewaju@yahoo.co.uk>
2022-04-06 14:36:41 +0300

    add database plugin

--- app/src/main/java/com/trivago/starwarsearch/StarWarSearchApplication.kt
+++ app/src/main/java/com/trivago/starwarsearch/StarWarSearchApplication.kt
@@ -8,6 +8,7 @@ import androidx.multidex.MultiDexApplication
 import com.facebook.flipper.android.AndroidFlipperClient
 import com.facebook.flipper.android.utils.FlipperUtils
 import com.facebook.flipper.plugins.crashreporter.CrashReporterPlugin
+import com.facebook.flipper.plugins.databases.DatabasesFlipperPlugin
 import com.facebook.flipper.plugins.inspector.DescriptorMapping
 import com.facebook.flipper.plugins.inspector.InspectorFlipperPlugin
 import com.facebook.soloader.SoLoader
@@ -67,6 +68,7 @@ class StarWarSearchApplication : MultiDexApplication() {
             val client = AndroidFlipperClient.getInstance(this)
             client.addPlugin(InspectorFlipperPlugin(this, DescriptorMapping.withDefaults()))
             client.addPlugin(CrashReporterPlugin.getInstance())
+            client.addPlugin(DatabasesFlipperPlugin(this))
             client.start()
         }
     }
